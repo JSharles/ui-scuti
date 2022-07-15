bb52d4edb72f10bc4aa8c5f4be997cdefe87bc52
JSharles <jeancharlesbarq@gmail.com>
2022-07-15 14:46:12 +0200

    fix: 🐛 fixing CI issue on PR

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -2,15 +2,15 @@ import React, { FC, HTMLAttributes } from 'react';
 //import './Page.style.css';
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
-    /** prop description goes here */
-};
+  /** prop description goes here */
+}
 
-export const Page: FC<Props> = (props) => {
-    return (
-        <div>
-            <p>Page</p>
-        </div>
-    );
+export const Page: FC<Props> = props => {
+  return (
+    <div>
+      <p>Page</p>
+    </div>
+  );
 };
 
 export default Page;
